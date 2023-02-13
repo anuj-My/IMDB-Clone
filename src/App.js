e9630672eadb187222ae15d1b13b6789f9d3067d
Anuj <anuj89806@gmail.com>
2023-02-13 22:57:04 +0530

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Home from "./pages/Home";
